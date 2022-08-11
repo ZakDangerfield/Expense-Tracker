5440d6dab5f5c0f4e0aaf2fd6499cb17d17bc45d
ZakDangerfield <ZakDangerfield53@gmail.com>
2022-08-11 14:53:25 +0100

    Basic Start to Expense Tracker App

---
+++ components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card.js";
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <div className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">£{props.amount}</div>
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
